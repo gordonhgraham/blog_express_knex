5d53443a3cbfae1a5b368cadc2b472cbff30fdb7
Gordon H Graham <theboss@gordonhgraham.com>
2016-09-20 14:10:49 -0600

    add routes for get one, all, and create user

--- db/api.js
+++ db/api.js
@@ -1,5 +1,16 @@
+'use strict';
+
 const knex = require(`./knex`)
 
 module.exports = {
-  // here we shall put all the stuff
+  getAllUsers() {
+    return knex('users');
+  },
+  getOneUser(id) {
+    return knex('users')
+    .where(`users.id`, id).first();
+  },
+  createOneUser(user) {
+    return knex('users').insert(user);
+  }
 }
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,38 @@ const router = express.Router();
 const db = require(`../db/api`);
 
 router.get(`/`, function(req, res) {
-  res.send(`this is the users route`);
+  db.getAllUsers().then(users => {
+    res.render(`users/users`, { users: users });
+  })
 });
+
+router.get(`/new`, function(req, res) {
+  res.render(`users/new`);
+});
+
+router.get(`/:id`, function(req, res) {
+  db.getOneUser(req.params.id).then(user => {
+    res.render(`users/user`, { user: user })
+  })
+});
+
+router.post(`/`, function(req, res) {
+  console.log(req.body);
+  db.createOneUser(req.body).then(() => {
+    res.redirect('/users')
+  });
+});
+
+// router.put(`/:id`, function(req, res) {
+//   res.send(`this will update a user`);
+// });
+
+// router.delete(`/:id`, function(req, res) {
+//   res.send(`this will delete a user`);
+// });
+
+// router.get(`/:id/edit`, function(req, res) {
+//   res.send(`this displays the edit form`);
+// });
 
 module.exports = router;
